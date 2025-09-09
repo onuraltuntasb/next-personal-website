0cf5200e0724871085779954c9502c2a0a395e58
onuraltunasb <onuraltuntasbusiness@gmail.com>
2025-09-09 12:14:02 +0300

    Update next.config.ts to use dynamic basePath and assetPrefix from environment variable

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const repoName = process.env.PAGES_BASE_PATH || '';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: repoName,
+  assetPrefix: repoName,
 };
 
 export default nextConfig;
